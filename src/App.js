018c8d256eea619741f5a32d75997321a902846b
Max Grechko <gremax@gremax.me>
2017-05-18 20:01:46 +0300

    Add StandardJS

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 const CITIES = [
-  { name: "Dnipro", id: "709930" },
-  { name: "Kyiv", id: "703448" },
-  { name: "Zaporizhzhya", id: "687700" },
-  { name: "Lviv", id: "702550" }
-];
+  { name: 'Dnipro', id: '709930' },
+  { name: 'Kyiv', id: '703448' },
+  { name: 'Zaporizhzhya', id: '687700' },
+  { name: 'Lviv', id: '702550' }
+]
 
 class WeatherDisplay extends Component {
-  constructor() {
-    super();
+  constructor () {
+    super()
     this.state = {
       weatherData: null
-    };
+    }
   }
-  componentDidMount() {
-    const cityId = this.props.city;
-    const Url = "http://api.openweathermap.org/data/2.5/weather?id=" + cityId + "&appid=47d81b69e5d0ff07a91c9c6bd7ca311e&units=metric";
+  componentDidMount () {
+    const cityId = this.props.city
+    const Url = 'http://api.openweathermap.org/data/2.5/weather?id=' + cityId + '&appid=47d81b69e5d0ff07a91c9c6bd7ca311e&units=metric'
     fetch(Url).then(res => res.json()).then(json => {
-      this.setState({ weatherData: json });
-    });
+      this.setState({ weatherData: json })
+    })
   }
-  render() {
-    const weatherData = this.state.weatherData;
-    if (!weatherData) return <div>Loading...</div>;
-    const weather = weatherData.weather[0];
-    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+  render () {
+    const weatherData = this.state.weatherData
+    if (!weatherData) return <div>Loading...</div>
+    const weather = weatherData.weather[0]
+    const iconUrl = 'http://openweathermap.org/img/w/' + weather.icon + '.png'
     return (
       <div>
         <h1>
@@ -38,26 +38,26 @@ class WeatherDisplay extends Component {
         <p>Low: {weatherData.main.temp_min}°C</p>
         <p>Wind Speed: {weatherData.wind.speed} m/s</p>
       </div>
-    );
+    )
   }
 }
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor () {
+    super()
     this.state = {
       activeCity: 0
-    };
+    }
   }
-  render() {
-    const activeCity = this.state.activeCity;
+  render () {
+    const activeCity = this.state.activeCity
     return (
-      <div className="App">
+      <div className='App'>
         {CITIES.map((city, index) => (
           <button
             key={index}
             onClick={() => {
-              this.setState({activeCity: index});
+              this.setState({activeCity: index})
             }}
           >
             {city.name}
@@ -68,8 +68,8 @@ class App extends Component {
           city={CITIES[activeCity].id}
         />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
